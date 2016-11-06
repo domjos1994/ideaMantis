018fd9a7e6a485eed14e8ae0abd62a11076d5c1c
dominic <dominic.joas@posteo.de>
2016-11-06 20:02:17 +0100

    add class for Object-Ref

---
+++ src/de/domjos/ideaMantis/soap/ObjectRef.java
@@ -0,0 +1,31 @@
+package de.domjos.ideaMantis.soap;
+
+import org.ksoap2.serialization.SoapObject;
+
+public class ObjectRef extends SoapObject {
+    private int id;
+    private String name;
+
+    public ObjectRef(String s, String s1) {
+        super(s, s1);
+        this.id = 0;
+        this.name = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
