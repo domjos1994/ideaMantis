7c6d1c86aec2736e9c98f337e783b2778af8cc34
dominic <dominic.joas@posteo.de>
2016-11-08 20:10:06 +0100

    update model

--- src/de/domjos/ideaMantis/model/MantisUser.java
+++ src/de/domjos/ideaMantis/model/MantisUser.java
@@ -1,54 +1,1 @@
-package de.domjos.ideaMantis.model;
-
-public class MantisUser {
-    private int id;
-    private String userName, name, email, password;
-
-    public MantisUser(String userName) {
-        this.id = 0;
-        this.userName = userName;
-        this.name = "";
-        this.email = "";
-        this.password = "";
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+package de.domjos.ideaMantis.model;public class MantisUser {    private int id;    private String userName, name, email, password;    public MantisUser(String userName) {        this.id = 0;        this.userName = userName;        this.name = "";        this.email = "";        this.password = "";    }    public int getId() {        return id;    }    public void setId(int id) {        this.id = id;    }    public String getUserName() {        return userName;    }    public void setUserName(String userName) {        this.userName = userName;    }    public String getName() {        return name;    }    public void setName(String name) {        this.name = name;    }    public String getEmail() {        return email;    }    public void setEmail(String email) {        this.email = email;    }    public String getPassword() {        return password;    }    public void setPassword(String password) {        this.password = password;    }}
